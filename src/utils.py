2b38654d7285e1662ef1c372d82195d73a6408bd
Kelly Christensen <kelly.christensen@sciencespo.fr>
2023-09-04 17:12:54 +0200

    add URAL regex for removing urls

--- src/utils.py
+++ src/utils.py
@@ -1,22 +1,18 @@
 # Credit to BERTweet. I've copied their TweetNormalizer and made some modifications.
 # https://github.com/VinAIResearch/BERTweet/blob/master/TweetNormalizer.py
 
-import emoji
-from nltk.tokenize import TweetTokenizer
 import re
 
+import emoji
+from nltk.tokenize import TweetTokenizer
+from ural.patterns import URL_IN_TEXT_RE
 
 tokenizer = TweetTokenizer()
 
 
 def normalizeToken(token):
-    lowercased_token = token.lower()
-    if token.startswith("@"):
-        return re.sub(r"@", "", token)
     if token.startswith("#"):
         return re.sub(r"#", "", token)
-    elif lowercased_token.startswith("http") or lowercased_token.startswith("www"):
-        return "HTTPURL"
     elif len(token) == 1 and emoji.is_emoji(token):
         return ""
     else:
@@ -29,11 +25,14 @@ def normalizeToken(token):
 
 
 def normalizer(tweet):
-    # Remove user citatation (i.e. "via @theregister")
-    tweet = re.sub(r"(?!http)via(\s{0,}@[a-z]*)", "", tweet)
-
     # Remove trailing white space
     tweet = tweet.strip()
+
+    # Separate titles / pre-colon spans from sentences
+    tweet = re.sub(r"(^(\w+\W+){1,2})*:", "\\1.", tweet)
+
+    # Remove URLs
+    tweet = URL_IN_TEXT_RE.sub(repl="", string=tweet)
 
     # Normalize apostrophes
     tokens = tokenizer.tokenize(tweet.replace("’", "'").replace("…", "..."))
@@ -41,22 +40,12 @@ def normalizer(tweet):
     # Clean tokens
     normTweet = " ".join([normalizeToken(token) for token in tokens])
 
-    # Normalize English-language conjunctions
-    normTweet = (
-        normTweet.replace("cannot ", "can not ")
-        .replace("n't ", " n't ")
-        .replace("n 't ", " n't ")
-        .replace("ca n't", "can't")
-        .replace("ai n't", "ain't")
-    )
-    normTweet = (
-        normTweet.replace("'m ", " 'm ")
-        .replace("'re ", " 're ")
-        .replace("'s ", " 's ")
-        .replace("'ll ", " 'll ")
-        .replace("'d ", " 'd ")
-        .replace("'ve ", " 've ")
-    )
+    # Remove user citatation (i.e. "via @theregister")
+    tweet = re.sub(r"(?!HTTPURL)via(\s{0,}@\w*)", "", tweet)
+
+    # Remove user handles
+    normTweet = re.sub(r"@", "", normTweet)
+
     normTweet = (
         normTweet.replace(" p . m .", "  p.m.")
         .replace(" p . m ", " p.m ")
