421944fabf667fc9333310a6a82faf429fb023b6
Kelly Christensen <kelly.christensen@sciencespo.fr>
2023-09-04 14:31:02 +0200

    update install

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,16 +1,17 @@
-# Install Python requirements
-pip install -r scripts/requirements.txt
+# # Install Python requirements
+# pip install -r scripts/requirements.txt
 
-# Install Hopsparser from source
-mkdir resources
-cd resources
-git clone https://github.com/kat-kel/hopsparser.git
-cd hopsparser
-pip install -e .
-pip install "hopsparser[spacy]"
+# # Install Hopsparser from source
+# mkdir resources
+# cd resources
+# git clone https://github.com/kat-kel/hopsparser.git
+# cd hopsparser
+# pip install -e .
+# pip install "hopsparser[spacy]"
 
 # Download Hopsparser French model
-MODEL_LOCATION="https://zenodo.org/record/7703346/files/UD_French-FTB-flaubert.tar.xz?download=1"
+MODEL_LOCATION="https://zenodo.org/record/7703346/files/UD_all_spoken_French-flaubert.tar.xz?download=1"
+echo "Making './models' directory"
 mkdir models
 cd models
 curl -o archive.tar.xz $MODEL_LOCATION
@@ -18,5 +19,3 @@ tar -xf archive.tar.xz
 rm archive.tar.xz
 cd ..
 
-# Install SpaCy French for Hopsparser
-python -m spacy download fr_core_news_sm
